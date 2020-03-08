e28499232a3fdc090547271591b340b5f8e4fcff
root <root@localhost.localdomain>
2020-03-08 12:19:01 +0630

    initial

---
+++ android/app/src/main/kotlin/com/example/yhh_opensource_calculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yhh_opensource_calculator
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
